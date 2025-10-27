70772fa32aed59dfd68374d828a05db6e8ed1223
Alexey Matskevich <github_job@mackevich.addymail.com>
2025-10-28 02:10:22 +0500

    Init repository

---
+++ lib/rubocop/rspec/guide.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "guide/version"
+
+module Rubocop
+  module Rspec
+    module Guide
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/rubocop/rspec/guide_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubocop::Rspec::Guide do
+  it "has a version number" do
+    expect(Rubocop::Rspec::Guide::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
